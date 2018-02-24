6c62596e48a6c61f98c240843bf13ee94610e12d
luisp <luisp@192.168.0.15>
2018-02-24 13:04:26 -0600

    Tarea 3 finalizada

--- src/ipc1/tarea3_201700995/IPC1Tarea3_201700995.java
+++ src/ipc1/tarea3_201700995/IPC1Tarea3_201700995.java
@@ -37,7 +37,7 @@ public class IPC1Tarea3_201700995 {
                  mayoramenor();//Se llama el método mayoramenor()
                  break;
              case 4:
-                
+                 Promedio();
                  break;
              case 5:
                  System.exit(0);//Se termina la ejecución del programa
@@ -96,13 +96,9 @@ public class IPC1Tarea3_201700995 {
                         }else
                               {
                                  m2=0;  
-                              }
-                 
-                 }
-                 
-                 
+                              }               
+                 }      
                  }while(m2==5);
-                
                  usuarios[i]=nombre; 
                  }
                  System.out.println("Gracias");
@@ -114,11 +110,8 @@ public class IPC1Tarea3_201700995 {
                  {                    
                  Usuarios(); //Se llama método del Usuarios()
                  }
-                 
-                 
                  break;
-                 
-             case 2:
+            case 2:
                  System.out.println("BIENVENIDO A MOSTRAR LOS USUARIOS ASCENDENTE");
                  for(int i=4;i>=0;i--)
                  {
@@ -159,8 +152,7 @@ public class IPC1Tarea3_201700995 {
      while(m==1);
         
     }
-    
-    
+        
     public static void Contador()//Método del contador
     {
         //Declaramos las variables a utilizar
@@ -168,7 +160,7 @@ public class IPC1Tarea3_201700995 {
      int opcioncont,contador,digitos,llave;
      digitos=1;//Será quién determine las decenas, centeas...
      contador=0;//Dirá la cantidad de dígitos que tiene el número
-     int m=0;//m sirve para repetir el ciclo si en caso el número ingresado es incorrecto.
+     int m;//m sirve para repetir el ciclo si en caso el número ingresado es incorrecto.
      //Escribimos el menú de CONTADOR DE NÚMEROS
      System.out.println("Bienvenido a 'CONTADOR DE NÚMEROS'"
              + "\nSeleccione una opción");
@@ -231,7 +223,7 @@ public class IPC1Tarea3_201700995 {
     {
        Scanner leer = new Scanner(System.in);
      int opcionmayoramenor,llave;
-     int m=0;
+     int m;
      System.out.println("Bienvenido a 'TRES NÚMEROS DE MAYOR A MENOR'"
              + "\nSeleccione una opción");
      System.out.println("1.Ingresar tres numeros"
@@ -323,5 +315,141 @@ public class IPC1Tarea3_201700995 {
          }
      }
      while(m==1); 
+    }
+    
+    public static void Promedio()//Método del contador
+    {
+        //Declaramos las variables a utilizar
+     Scanner leer = new Scanner(System.in);
+     int opcioncont,llave;
+     int promed=0;
+     int m;//m sirve para repetir el ciclo si en caso el número ingresado es incorrecto.
+     //Escribimos el menú de CONTADOR DE NÚMEROS
+     System.out.println("Bienvenido a 'CALCULAR PROMEDIO'"
+             + "\nSeleccione una opción");
+     System.out.println("1.Calcular Promedio"
+             + "\n2.Menú principal");
+     System.out.println("Ingrese un número");
+     do{
+         m=0;         
+     opcioncont = leer.nextInt();//Se lee el número a evaluar en el switch
+     switch(opcioncont){
+             case 1:
+                 //En esta parte se crean los arreglos y la matriz
+                int promedio[][]=new int[6][6];
+                 int alumno1[]= new int[6];
+                  int alumno2[]= new int[6];
+                   int alumno3[]= new int[6];
+                    int alumno4[]= new int[6];
+                     int alumno5[]= new int[6];
+                      int alumno6[]= new int[6];
+                 System.out.println("BIENVENIDO A CÁLCULO DE PROMEDIO");
+                 System.out.println("Solo puede calcular el promedio de 4 notas de 6 estudiantes");
+                 //En esta parte se pide al usuario ingresar los datos de cada alumno
+                 System.out.println("Ingrese el ID del primer estudiante");
+                 alumno1[0]=leer.nextInt(); 
+                 for(int j=1;j<=4;j++){
+                          System.out.println("ingrese una nota");
+                         alumno1[j]=leer.nextInt();
+                         promed=(promed+alumno1[j]);
+                 }                
+                 alumno1[5]=promed/4;
+                 promed=0;
+                 System.out.println("Ingrese el ID del segundo estudiante");
+                 alumno2[0]=leer.nextInt();
+                
+                 for(int j=1;j<5;j++){
+                          System.out.println("ingrese una nota");
+                         alumno2[j]=leer.nextInt();  
+                         promed=(promed+alumno2[j]);
+                 }  
+                 alumno2[5]=promed/4;
+                 promed=0;
+                 System.out.println("Ingrese el ID del tercer estudiante");
+                 alumno3[0]=leer.nextInt(); 
+                 for(int j=1;j<5;j++){
+                          System.out.println("ingrese una nota");
+                         alumno3[j]=leer.nextInt(); 
+                         promed=(promed+alumno3[j]);
+                 } 
+                 alumno3[5]=promed/4;
+                 promed=0;
+                 System.out.println("Ingrese el ID del cuarto estudiante");
+                 alumno4[0]=leer.nextInt(); 
+                 for(int j=1;j<5;j++){
+                          System.out.println("ingrese una nota");
+                         alumno4[j]=leer.nextInt(); 
+                         promed=(promed+alumno4[j]);
+                 } 
+                 alumno4[5]=promed/4;
+                 promed=0;
+                 System.out.println("Ingrese el ID del quinto estudiante");
+                 alumno5[0]=leer.nextInt();  
+                 for(int j=1;j<5;j++){
+                          System.out.println("ingrese una nota");
+                         alumno5[j]=leer.nextInt(); 
+                         promed=(promed+alumno5[j]);
+                 } 
+                 
+                 alumno5[5]=promed/4;
+                 promed=0;
+                 System.out.println("Ingrese el ID del sexto estudiante");
+                 alumno6[0]=leer.nextInt(); 
+                 for(int j=1;j<5;j++){
+                          System.out.println("ingrese una nota");
+                         alumno6[j]=leer.nextInt(); 
+                         promed=(promed+alumno6[j]);
+                 }   
+                  alumno6[5]=promed/4;
+                 promed=0;
+                 //En esta parte se asignan los valores contenidos en los arreglos a la matriz
+        int fila;
+        int columna;
+        for(fila = 0; fila<=6; fila++){
+            for(columna = 0; columna<6; columna++){
+                if(fila==0){
+                promedio[fila][columna]=alumno1[columna];
+                }else if(fila==1){
+               promedio[fila][columna]=alumno2[columna];
+                }else if(fila==2){
+                promedio[fila][columna]=alumno3[columna];
+                }else if(fila ==3){
+                promedio[fila][columna]=alumno4[columna];
+                }else if(fila ==4){
+                promedio[fila][columna]=alumno5[columna];
+                }else if(fila ==5){
+                promedio[fila][columna]=alumno6[columna];
+                }
+            }
+        }  
+        //Aquí se escribe o imprime en pantalla tosos los datos almacenados en la matris incluyendo el promedio ya calculado
+        System.out.println("\t"+"ID    "+" Nota1 "+"  Nota2 "+"  Nota3 "+"  Nota4 "+"  Promedio ");
+        for(fila = 0; fila<promedio.length; fila++){
+             
+            for(columna = 0; columna<promedio.length; columna++){
+                System.out.print("\t"+promedio[fila][columna]+" ");    
+            }
+             System.out.println("");
+        }
+                 System.out.println("Gracias");
+                 System.out.println("");
+                 System.out.println("1. Regresar");
+                 llave=leer.nextInt();//Se lee la variable llave
+                 System.out.println("");
+                 if(llave==1)//La variable llave sirve para leer el numero si el usuario desea regresar al menú anterior
+                 {                    
+                 Contador(); //Se llama método del Contador()
+                 }  
+                 break;
+             case 2:
+                 System.out.println("");
+                menu();//Se llama al método menu()           
+                 break;
+             default:
+                m=1;// Si en caso el número ingresado fuera incorrecto m sera igual a 1 y el ciclo se repetirá.
+                System.out.println("Intente de nuevo");
+         }
+     }
+     while(m==1);        
     }
 }
